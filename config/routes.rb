a96a4d9ee0a1202e0034ee9e0f8ba645fad4909a
Harshal <you@example.com>
2017-01-17 17:08:04 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :authors
+get 'posts/:id' => 'posts#show'
+  get 'posts/show'
+
+  get 'home/about'
+
+get 'about'=> 'home#about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
